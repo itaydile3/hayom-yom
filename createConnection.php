88c4bfe0807816a22b5967c6db3382d4f44e153d
Itay Cohen <itay.cohen@solaredge.com>
2020-07-14 00:25:25 +0300

    set names utf 8

--- createConnection.php
+++ createConnection.php
@@ -8,7 +8,7 @@ header('Content-Type: text/html; charset=utf-8');
 
 // Create connection
 $conn = new mysqli($servername, $username, $password, $database);
-
+$conn->query("set names 'utf8'");
 // Check connection
 if ($conn->connect_error) {
   die("Connection failed: " . $conn->connect_error);
