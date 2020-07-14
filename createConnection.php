45197b7a401c0f213e96e5e41fe6863cd7b049b8
Itay Cohen <itay.cohen@solaredge.com>
2020-07-14 11:18:10 +0300

    more css adjustments

--- createConnection.php
+++ createConnection.php
@@ -4,6 +4,15 @@ $username = "minute4soul";
 $password = "jghj3455FHG765Fjh576567@jlkjkljkl";
 $database = "minute4soul";
 
+$actual_link = "http://$_SERVER[HTTP_HOST]$_SERVER[REQUEST_URI]";
+
+if (strpos($actual_link, 'hayom-yom') > -1){
+  $servername = "localhost";
+  $username = "root";
+  $password = "";
+  $database = "hayom";
+}
+
 header('Content-Type: text/html; charset=utf-8');
 
 // Create connection
